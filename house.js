643986aa012a332d66dc143abab127f81e960ba4
vaibhavyxx <vd5443@rit.edu>
2023-07-12 15:22:16 +0530

    pushed basics of garden
    issue with 3d model not uploading as intended

---
+++ house.js
@@ -0,0 +1,21 @@
+AFRAME.registerGeometry('house', {
+  schema: {
+
+  },
+
+  init: function () {
+
+    /*const scene = document.querySelector('a-scene')
+    const house = document.createElement('#house')
+    house.classList.add('house');
+    scene.appendChild('house');
+
+    for(let i=0; i < 10; i++){
+      let tinyHouse = document.createElement('#house');
+      tinyHouse.setAttribute('position', {x: i+10, y: i+10, z:0})
+      house.appendChild(tinyHouse)
+    }
+*/
+    
+  }
+});
